98950aeb854265af50586e38c253bfc60e315798
ealande <erickalan23@gmail.com>
2023-07-12 23:54:34 -0300

    styles added and some minor issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Form from "./components/form/index"
+import GlobalStyle from "./components/styles"
+
 
 function App() {
 
   return (
   <>
+  <GlobalStyle />
   <Form />
   </>)
 }
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,4 +1,26 @@
 import { useState } from "react";
+import styled from 'styled-components'
+
+const Resultado = styled.p`
+text-align:center;
+padding: 1px;
+margin-top: 15px;
+`
+const Borda = styled.div`
+border: solid;
+margin-top: 10em;
+margin-right: 15%;
+margin-left: 15%;
+min-width: 70%;
+display:flex;
+flex-direction: column;
+align-items: flex-start;`
+
+const Wave = styled.div`
+position: absolute;
+height: 70px;
+width: 600px;
+background: #e0efe3;`
 
 const Form = () => {
 
@@ -17,23 +39,33 @@ const Form = () => {
             return (
             <p>Seu IMC é considerado Normal</p>
             )
+        } else if (imc > 25){
+            return(
+                <p>Seu IMC é considerado Obeso</p>
+            )
         } else {
             return (
-            <p>Seu IMC é considerado Obeso</p>
+            <p></p>
             )
         }
     }
-    
+    const IMC = weight / height;
+
     return (
+        <>
+        <h1>Calculadora IMC</h1>
         <form>
-            <input type ="number" step="0.01"placeholder="Height" onChange={evento => setHeight(parseFloat(evento.target.value))}/>
-            <input type ="number" step="0.1" placeholder="Weight" onChange={evento => setWeight(parseFloat(evento.target.value))} />
-            {renderResult()}
-            <p>Altura:</p> {height} <br/>
-            <p>Peso:</p> {weight}<br/>
-            <p>IMC:</p> {weight / height}
+            <input type ="number" step="0.01"placeholder="Altura" onChange={evento => setHeight(parseFloat(evento.target.value))}/>
+            <input type ="number" step="0.1" placeholder="Peso" onChange={evento => setWeight(parseFloat(evento.target.value))} />
+            <Borda>
+            <Resultado>{renderResult()}</Resultado>
+
+            <Resultado>Altura:{height} </Resultado> <br/>
+            <Resultado>Peso: {weight} </Resultado><br/>
+            <Resultado>IMC: {IMC} </Resultado>
+            </Borda>
         </form>
-        
+        </>
     )
 }
 
